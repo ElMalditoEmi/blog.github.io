f7b1a4e9f9a55ce55848bf33fe1f69d515bd7742
emiemiemi <emijoaco_pereyra@hotmail.com>
2024-11-21 20:04:38 -0300

    Gato responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <div className="footer p-4 w-full fixed bottom-0 flex flex-col items-center">
       <a href="https://github.com/ElMalditoEmi" target="_blank" rel="noopener noreferrer">
-        <Image src="/github-logo.svg" alt="GitHub Logo" width={24} height={24} />
+        <Image src="/github-logo.svg" alt="GitHub Logo" width={24} height={24} className='sm:w-8 sm:h-8 w-6 h-6'/>
       </a>
       <p className='pt-2'>&copy; {new Date().getFullYear()} ElMalditoEmi. All rights reserved.</p>
     </div>
