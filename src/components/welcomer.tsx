b1300295887df9c53c1a4409442b5d89f8928eb3
emiemiemi <emijoaco_pereyra@hotmail.com>
2024-11-21 00:53:08 -0300

    Changes for deploy

--- src/components/welcomer.tsx
+++ src/components/welcomer.tsx
@@ -4,7 +4,7 @@ const Welcomer: React.FC = () => {
   return (
     <div className="bg-background text-foreground p-12 rounded-lg align-middle padding">
       <h1 className="text-7xl font-bold pt-10">Welcome</h1>
-      <h3>To ElMalditoEmi's page. :)</h3>
+      <h3>To ElMalditoEmi&apos;s page. :)</h3>
     </div>
   );
 };
